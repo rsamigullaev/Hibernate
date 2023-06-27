008bc119a52601173b31ce27d17d43f505d2e59e
samelena <elena_belle@mail.ru>
2023-06-27 21:56:53 +0300

    add commit

---
+++ src/test/java/ru/netology/hibernate/HibernateApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.netology.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class HibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
